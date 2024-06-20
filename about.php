845257ae9b3b019986b963a8ddd0baa04dcd4100
CynthiaWKamau <cynthia.kamau@strathmore.edu>
2024-06-20 11:04:38 +0300

    converted html files to php

--- about.php
+++ about.php
@@ -16,17 +16,7 @@
      
       </div>
   <header>
-      <div class="navlinks">
-          <ul id="menulist">
-              <li><a href="index.html">Home page</a></li>
-              <li><a href="about.html">About us</a></li>
-              <li><a href="accomodation.html">Accomodation details</a></li>
-              <li> <a href="rooms.html">Rooms Details</a></li>
-              <li><a href="menu.html">Menu</a></li>
-              <li><a href="Activities.html">Activities</a></li>
-              <li><a href="bookings.html">Book Services</a></li>    
-          </ul>
-      </div>
+    <?php include_once("templates/nav.php");?>    
       </div>
     </header>
 
