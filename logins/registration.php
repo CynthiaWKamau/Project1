3539e9659e59c8a2535c814b58344e5a7a06dd46
CynthiaWKamau <cynthia.kamau@strathmore.edu>
2024-06-25 12:01:05 +0300

    changes

--- logins/registration.php
+++ logins/registration.php
@@ -1,5 +1,30 @@
 <?php
-    include 'db_connection.php';
+require "config.php";
+include 'db_connection.php';
+
+if(isset($_POST["submit"])) {   
+    $name = $_POST["name"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmpassword = $_POST["confirmpassword"];
+    
+    // Check for duplicate username or email
+    $duplicate = mysqli_query($conn, "SELECT * FROM tb_user WHERE username='$username' OR email='$email'");
+    
+    if(mysqli_num_rows($duplicate) > 0) {
+        echo "<script>alert('Username or Email Has Already Been Taken');</script>";
+    } else {
+        if($password == $confirmpassword) {
+            // Insert the new user into the database
+            $query = "INSERT INTO tb_user (name, username, email, password) VALUES ('$name', '$username', '$email', '$password')";
+            mysqli_query($conn, $query);
+            echo "<script>alert('Registration Successful');</script>";
+        } else {
+            echo "<script>alert('Password Does Not Match');</script>";
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -11,14 +36,25 @@
 </head>
 <body>
     <h2>Registration</h2>
-    <form class=""action="" method="post" autocomplete="">
+    <form action="" method="post" autocomplete="off">
         <label for="name">Name:</label>
-        <input type="text" name="name" id="name" required value=""> <br>
-        <label for="username">Username:</label>
-        <input type="text" name="username" id="username" required value=""> <br>
-        <label for="email">Email:</label>
-        <input type="text" name="email" id="email" required value=""> <br>
+        <input type="text" name="name" id="name" required><br>
         
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username" required><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required><br>
+        
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" required><br>
+        
+        <label for="confirmpassword">Confirm Password:</label>
+        <input type="password" name="confirmpassword" id="confirmpassword" required><br>
+        
+        <button type="submit" name="submit">Register</button>
     </form>
+    <br>
+    <a href="login.php">Login</a>
 </body>
 </html>
