738c3ac269f2ef7ef2c65dfa0c5822d4b5471cab
CynthiaWKamau <cynthia.kamau@strathmore.edu>
2024-06-26 13:57:48 +0300

    changes

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<?php
-require 'config.php';
-if(!empty($_SESSION["id"])){
-    $id=$_SESSION["id"];
-    $result=mysqli_query($conn,"SELECT*FROM tb_user WHERE id=$id");
-    $row=mysqli_fetch_array($result);
-}
-else{
-    header("Location:login.php");
-}
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
