105c99d90a9e64118bb2889325b5402eb80d0d6d
CynthiaWKamau <cynthia.kamau@strathmore.edu>
2024-07-02 12:01:26 +0300

    customization of sign in pages

--- signin.php
+++ signin.php
@@ -56,6 +56,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Registration</title>
+    <link rel="stylesheet" href="signin.css">
+
 </head>
 <body>
     <div class="container">
