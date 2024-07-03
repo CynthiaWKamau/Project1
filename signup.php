770d9ca18336b92cbe0339b799c36a1074da1d17
CynthiaWKamau <cynthia.kamau@strathmore.edu>
2024-07-03 10:49:53 +0300

    delete user code

--- signup.php
+++ signup.php
@@ -92,7 +92,74 @@
             }
         }
     }
- 
+  // Update user
+  if (isset($_POST["update"])) {
+    if (isset($_POST["update_username"])) {
+        $update_username = $_POST["update_username"];
+        $update_name = $_POST["update_name"] ?? null;
+        $update_email = $_POST["update_email"] ?? null;
+        $update_password = $_POST["update_password"] ?? null;
+
+        $query = "UPDATE registration SET ";
+        $params = [];
+        $types = "";
+
+        if ($update_name) {
+            $query .= "name=?, ";
+            $params[] = $update_name;
+            $types .= "s";
+        }
+        if ($update_email) {
+            $query .= "email=?, ";
+            $params[] = $update_email;
+            $types .= "s";
+        }
+        if ($update_password) {
+            $query .= "password=?, ";
+            $params[] = $update_password;
+            $types .= "s";
+        }
+
+        if (count($params) > 0) {
+            $query = rtrim($query, ", ");
+            $query .= " WHERE username=?";
+            $params[] = $update_username;
+            $types .= "s";
+
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param($types, ...$params);
+
+            if ($stmt->execute()) {
+                echo "<script>alert('User updated successfully');</script>";
+            } else {
+                echo "<script>alert('Update failed');</script>";
+            }
+
+            $stmt->close();
+        } else {
+            echo "<script>alert('No changes provided');</script>";
+        }
+    }
+}
+
+        // Delete user
+        if (isset($_POST["delete"])) {
+            if (isset($_POST["delete_username"])) {
+                $delete_username = $_POST["delete_username"];
+
+                $stmt = $conn->prepare("DELETE FROM registration WHERE username=?");
+                $stmt->bind_param("s", $delete_username);
+
+                if ($stmt->execute()) {
+                    echo "<script>alert('User deleted successfully');</script>";
+                } else {
+                    echo "<script>alert('Delete failed');</script>";
+                }
+
+                $stmt->close();
+            }
+        }
+    
     ?>
 </body>
 </html>
